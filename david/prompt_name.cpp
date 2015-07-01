16f87ac3fcd781926145a4238bf0e6af133ae86c
David Li <li.davidm96@gmail.com>
2015-06-30 19:59:35 -0500

    Implement exercise 3 and tests

---
+++ david/prompt_name.cpp
@@ -0,0 +1,18 @@
+/**
+ * prompt_name.cpp - a program that prompts the user for their name and
+ * echoes it back to them
+ */
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+using namespace std::literals;
+
+int main() {
+    string user_name;
+    cout << "Please enter your name: "s;
+    cin >> user_name;
+    cout << "Hello, " << user_name << "!\n"s;
+    return EXIT_SUCCESS;
+}
