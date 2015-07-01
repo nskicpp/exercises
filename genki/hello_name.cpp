38ecdd63bbaea96a80a6769cc7227a4d0e04c983
Genki Marshall <genkimarshall@gmail.com>
2015-06-30 19:24:45 -0700

    [lesson 3] Fix various style issues after review
    - Explicitly include headers used, to confirm to C++ Standards,
      clarify what is being used, etc.
    - Bring the use of the namespace into only-function scope, to
      follow the idea of minimal-scope.
    - Declare the `name` variable only right when it is needed to be used,
      because it's generally good style, and in this case, it looks visually
      appealing when aligned this way :).
    - Clarify to user what the format of their name should be, because
      standard-in is space-delimited.
    - Switch away from K&R style { because in C++, like elsewhere, the
      consensus is there is no consensus, there is no style being used
      elsewhere in this code, and I prefer in-line.
    - Rename project title to clarify purpose.
    
    Signed-off-by: Patrick M. Niedzielski <patrick@pniedzielski.net>

--- genki/hello_name.cpp
+++ genki/hello_name.cpp
@@ -1,18 +1,21 @@
 /* hello_name.cpp
 
-   Get a user's name, then print them a message.
+   Get a user's first name, then print them a message.
 */
 
+#include <string>
+#include <cstdlib>
 #include <iostream>
-using namespace std;
 
-int main()
-{
-    string name;
+int main() {
+    using namespace std::literals;
 
-    cout << "Please enter your name:\n"s;
-    cin  >> name;
-    cout << "Hi "s << name << "!\n"s;
+    std::cout << "Please enter your first name, no spaces:\n"s;
+
+    std::string name;
+    std::cin >> name;
+
+    std::cout << "Hi "s << name << "!\n"s;
 
     return EXIT_SUCCESS;
 }
