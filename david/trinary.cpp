32d1cb27da0499d98ac2f61f86138d09a45f247a
David Li <li.davidm96@gmail.com>
2015-07-16 21:41:41 -0500

    Restructure trinary parser to make error handling clearer
    License: Boost

--- david/trinary.cpp
+++ david/trinary.cpp
@@ -15,30 +15,29 @@ using namespace std::literals;
 
 int main() {
     auto input = ""s;
-    if (std::cin >> input) {
-        auto result = 0;
-        for (auto c : input) {
-            if (c == '0') {
-                result *= 3;
-            }
-            else if (c == '1') {
-                result *= 3;
-                result += 1;
-            }
-            else if (c == '2') {
-                result *= 3;
-                result += 2;
-            }
-            else {
-                std::cerr << "Invalid trinary digit: "s << c << std::endl;
-                return EXIT_FAILURE;
-            }
-        }
-        std::cout << result;
-        return EXIT_SUCCESS;
-    }
-    else {
+    std::cin >> input;
+    if (!std::cin) {
         std::cerr << "No digits read.\n"s;
         return EXIT_FAILURE;
     }
+    auto result = 0;
+    for (auto c : input) {
+        if (c == '0') {
+            result *= 3;
+        }
+        else if (c == '1') {
+            result *= 3;
+            result += 1;
+        }
+        else if (c == '2') {
+            result *= 3;
+            result += 2;
+        }
+        else {
+            std::cerr << "Invalid trinary digit: "s << c << std::endl;
+            return EXIT_FAILURE;
+        }
+    }
+    std::cout << result;
+    return EXIT_SUCCESS;
 }
